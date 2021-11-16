8d0e96f170b94792d2a1f96f012387844d9f0a6d
ashrafmakandar <ashraf9587@gmail.com>
2021-11-16 22:29:24 +0530

    github actions added

--- lib/Home.dart
+++ lib/Home.dart
@@ -9,11 +9,6 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
